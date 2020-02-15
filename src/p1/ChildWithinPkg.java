c917df3884e69f1bcb2bc54b3f3356763bdf789f
manoj <manoj.sarwate@ssiedu.in>
2020-02-15 19:26:46 +0530

    15-02-2020

---
+++ src/p1/ChildWithinPkg.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class ChildWithinPkg extends Main {
+    public void check(){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+       // System.out.println(d);
+    }
+}
---
+++ src/p2/ChildOutsidePkg.java
@@ -0,0 +1,10 @@
+package p2;
+
+public class ChildOutsidePkg extends p1.Main {
+    public void printData(){
+        System.out.println(a);
+        System.out.println(b);
+        //System.out.println(c);
+        //System.out.println(d);
+    }
+}
