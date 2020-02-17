915336d2d18844a1a69fe70c41813c98b4545530
manoj <manoj.sarwate@ssiedu.in>
2020-02-17 18:57:00 +0530

    17-02-2020

--- src/banking/ATMMachine.java
+++ src/banking/ATMMachine.java
@@ -3,6 +3,25 @@ package banking;
 public class ATMMachine {
 
     public static void printInfo(Card card){
+        
+        if(card instanceof HDFCCard){
+            System.out.println("Its An HDFC Card");
+        }else if(card instanceof SBICard){
+            System.out.println("Its An SBI Card");
+        }else if(card instanceof ICICICard){
+            System.out.println("Its An ICICI Card");
+        }
+        
+        /*
+        String name=card.getClass().getSimpleName();
+        if(name.equals("HDFCCard")){
+            System.out.println("Its An HDFC Card");
+        }else if(name.equals("SBICard")){
+            System.out.println("Its An SBI Card");
+        }else if(name.equals("ICICICard")){
+            System.out.println("Its An ICICI Card");
+        }
+        */
         System.out.println("Acc No  : "+card.getAccountNo());
         System.out.println("Balance : "+card.getBalance());
     }
@@ -10,8 +29,8 @@ public class ATMMachine {
         SBICard sbi=new SBICard("SB1110",10000);
         HDFCCard hdfc=new HDFCCard("HDFC5343",20000);
         ICICICard icici=new ICICICard("IC124323",49000);
-        printInfo(sbi);
-        printInfo(hdfc);
+        //printInfo(sbi);
+        //printInfo(hdfc);
         printInfo(icici);
     }
     
